09b84d1ed07036d9a5fb9606e55a443d33a5968b
Leonardo Pignataro <leopignataro@gmail.com>
2016-04-18 22:27:15 -0300

    PHP standard: Added example of configuring a container for the text on the PAdES sample

--- PHP/standard/pades-signature.php
+++ PHP/standard/pades-signature.php
@@ -136,8 +136,17 @@ $signatureStarter->setVisualRepresentation([
 		'includeSigningTime' => true,
 
 		// Optionally set the horizontal alignment of the text ('Left' or 'Right'), if not set the default is Left
-		'horizontalAlign' => 'Left'
+		'horizontalAlign' => 'Left',
 
+		// Optionally set the container within the signature rectangle on which to place the text. By default, the
+		// text can occupy the entire rectangle (how much of the rectangle the text will actually fill depends on the
+		// length and font size). Below, we specify that the text should respect a right margin of 1.5 cm.
+		'container' => [
+			'left' => 0,
+			'top' => 0,
+			'right' => 1.5,
+			'bottom' => 0
+		]
 	],
 
 	'image' => [
