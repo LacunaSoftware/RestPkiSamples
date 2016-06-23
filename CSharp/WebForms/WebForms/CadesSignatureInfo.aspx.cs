8163927bb91d079ab31944bd090ec4f21e5ddd46
Leonardo Pignataro <leopignataro@gmail.com>
2016-06-23 20:39:15 -0300

    C#: adding sample project using ASP.NET Web Forms

---
+++ CSharp/WebForms/WebForms/CadesSignatureInfo.aspx.cs
@@ -0,0 +1,23 @@
+﻿using Lacuna.RestPki.Api;
+using Lacuna.RestPki.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebForms {
+
+	public partial class CadesSignatureInfo : System.Web.UI.Page {
+
+		protected PKCertificate signerCertificate;
+		protected string signatureFilename;
+
+		protected void Page_Load(object sender, EventArgs e) {
+			this.signatureFilename = PreviousPage.SignatureFilename;
+			this.signerCertificate = PreviousPage.SignerCertificate;
+		}
+
+	}
+}
