78b8ac9809471793b4f00591921426dc4a76f686
Leonardo Pignataro <leopignataro@gmail.com>
2016-06-30 15:07:02 -0300

    PHP: minor changes

--- PHP/legacy52/util.php
+++ PHP/legacy52/util.php
@@ -12,7 +12,7 @@ function getRestPkiClient() {
 
 	// Throw exception if token is not set (this check is here just for the sake of newcomers, you can remove it)
 	if (strpos($restPkiAccessToken, ' API ') !== false) {
-		throw new Exception('The API access token was not set! Hint: to run this sample you must generate an API access token on the REST PKI website and paste it on the file api/util.php');
+		throw new Exception('The API access token was not set! Hint: to run this sample you must generate an API access token on the REST PKI website and paste it on the file util.php');
 	}
 
 	// -----------------------------------------------------------------------------------------------------------
