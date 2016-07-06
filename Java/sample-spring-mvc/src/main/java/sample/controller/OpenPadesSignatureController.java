796cb168079c2ca1657240f0629a52189ab7965c
Leonardo Pignataro <leopignataro@gmail.com>
2016-07-06 00:01:13 -0300

    Java: changed default validation parameters for PAdES open signature example

--- Java/sample-spring-mvc/src/main/java/sample/controller/OpenPadesSignatureController.java
+++ Java/sample-spring-mvc/src/main/java/sample/controller/OpenPadesSignatureController.java
@@ -37,7 +37,7 @@ public class OpenPadesSignatureController {
         // Parameters for the signature validation. We have encapsulated this code in a method to include several
         // possibilities depending on the argument passed. Experiment changing the argument to see different validation
         // configurations. Once you decide which is best for your case, you can place the code directly here.
-        setValidationParameters(sigExplorer, 2);
+        setValidationParameters(sigExplorer, 1);
         // try changing this number ---------^ for different validation parameters
 
         // Call the open() method, which returns the signature file's information
