d72b31bdc6a524d25aadcbc1228abc6ffc37b9fb
Leonardo Pignataro <leopignataro@gmail.com>
2017-03-22 15:51:13 -0300

    C# VS2008: batch signature sample

--- CSharp/VS2008/WebApplication1/Default.aspx
+++ CSharp/VS2008/WebApplication1/Default.aspx
@@ -26,13 +26,7 @@
 				<li><a href="/XmlElementSignature.aspx">XML element signature</a></li>
 			</ul>
 		</li>
-		<%--<li>
-			Sign a batch of files
-			<ul>
-				<li><a href="/BatchSignature">Simple batch signature</a></li>
-				<li><a href="/BatchSignatureOptimized">Optimized batch signature</a> (better performance but more complex Javascript)</li>
-			</ul>
-		</li>--%>
+		<li><a href="/BatchSignature.aspx">Sign a batch of files</a></li>
 	</ul>
 
 </asp:Content>
--- CSharp/VS2008/WebApplication1/Site.Master.designer.cs
+++ CSharp/VS2008/WebApplication1/Site.Master.designer.cs
@@ -41,6 +41,15 @@ namespace WebApplication1 {
         protected global::System.Web.UI.HtmlControls.HtmlForm Form1;
         
         /// <summary>
+        /// ScriptManager1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.ScriptManager ScriptManager1;
+        
+        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
