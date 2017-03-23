3316fca7abac4657ae20fb606bebfbcf083864a4
Leonardo Pignataro <leopignataro@gmail.com>
2017-03-22 22:59:00 -0300

    C# MVC: centralized configuration of endpoints and licenses

--- CSharp/MVC/SampleSite/Classes/Util.cs
+++ CSharp/MVC/SampleSite/Classes/Util.cs
@@ -12,11 +12,14 @@ namespace SampleSite.Classes {
 
 		public static RestPkiClient GetRestPkiClient() {
 			var accessToken = ConfigurationManager.AppSettings["RestPkiAccessToken"];
-			// Throw exception if token is not set (this check is here just for the sake of newcomers, you can remove it)
 			if (string.IsNullOrEmpty(accessToken) || accessToken.Contains(" API ")) {
 				throw new Exception("The API access token was not set! Hint: to run this sample you must generate an API access token on the REST PKI website and paste it on the web.config file");
 			}
-			return new RestPkiClient("https://pki.rest/", ConfigurationManager.AppSettings["RestPkiAccessToken"]);
+			var endpoint = ConfigurationManager.AppSettings["RestPkiEndpoint"];
+			if (string.IsNullOrEmpty(endpoint)) {
+				endpoint = "https://pki.rest/";
+			}
+			return new RestPkiClient(endpoint, accessToken);
 		}
 
 		public static string ContentPath {
